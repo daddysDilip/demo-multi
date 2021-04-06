3c4ffada97a6c3344a1b3655fcf03dca04df0284
daddysDilip <dilip.daddyscode@gmail.com>
2021-04-06 18:01:11 +0530

    make change for design

--- project/resources/views/sadmin/userlist.blade.php
+++ project/resources/views/sadmin/userlist.blade.php
@@ -1,116 +1,61 @@
-@extends('sadmin.includes.master-sadmin')
-
+@extends('sadmin.includes.master-sadmin2')
 @section('content')
-
-    <div class="prtm-content-wrapper">
-        <div class="prtm-content">
-            <div class="prtm-page-bar">
-                <ul class="breadcrumb">
-                    <li class="breadcrumb-item text-cepitalize">
-                        <h3>User Section</h3> </li>
-                    <li class="breadcrumb-item"><a href="{!! url('sadmin/dashboard') !!}">Home</a></li>
-                    <li class="breadcrumb-item">User Section</li>
-                </ul>
-            </div>
-
+<div class="block-header">
+  <div class="row">
+    <div class="col-lg-7 col-md-6 col-sm-12">
+      <h2>User Section</h2>
+    </div>
+    <div class="col-lg-5 col-md-6 col-sm-12">
+      <ul class="breadcrumb float-md-right">
+        <li class="breadcrumb-item"><a href="{!! url('sadmin/dashboard') !!}"><i class="zmdi zmdi-home"></i> Home</a></li>
+        <li class="breadcrumb-item active">User Section</li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
             <!-- Page Content -->
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div id="res">
-                        @if(Session::has('message'))
-                            <div class="alert alert-success alert-dismissable">
-                                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                                {{ Session::get('message') }}
-                            </div>
-                        @endif
-                    </div>
-                    <div class="prtm-block-title mrgn-b-lg">
-                        <div class="caption">
-                            <a href="{!! url('sadmin/user/create') !!}" class="btn btn-primary btn-add"><i class="fa fa-plus"></i> Add New User</a>
-                        </div>
-                    </div>
-                    <!--  <table class="table table-striped table-bordered" cellspacing="0" id="example" width="100%">
-                        <thead>
-                            <tr class="bg-primary">
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($user as $alluser)
-                            <tr>
-                                <td>{{$alluser->name}}</td>
-                                <td>{{$alluser->email}}</td>
-                                <td>{{$alluser->phone}}</td>
-                               
-                                <td>
-                                    @if($alluser->status == 1)
-                                        <a href="{!! url('sadmin/user') !!}/status/{{$alluser->id}}/0" class="btn btn-success btn-xs">Active</a>
-                                    @elseif($alluser->status == 0)
-                                        <a href="{!! url('sadmin/user') !!}/status/{{$alluser->id}}/1" class="btn btn-danger btn-xs">Deactive</a>
-                                    @endif
-                                </td>
-                                <td>
-                                    <div class="dropdown display-ib">
-                                        <a href="javascript:;" class="mrgn-l-xs" data-toggle="dropdown" data-hover="dropdown" data-close-others="true" aria-expanded="false"><i class="fa fa-cog fa-lg base-dark"></i></a>
-                                        <ul class="dropdown-menu dropdown-arrow dropdown-menu-right">
-                                            <li>
-                                                <a href="user/{{$alluser->id}}/edit"><i class="fa fa-edit"></i> <span class="mrgn-l-sm">Edit </span> </a>
-                                            </li>
-                                            <li>
-                                                <a href="#" onclick="return delete_data('{{$alluser->id}}');"><i class="fa fa-trash"></i> <span class="mrgn-l-sm">Delete </span></a>
-                                            </li>
-                                        </ul>  
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table> -->
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <div id="res">
+        @if(Session::has('message'))
+          <div class="alert alert-success alert-dismissable">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+              {{ Session::get('message') }}
+          </div>
+        @endif
+      </div>
+      <div class="prtm-block-title mrgn-b-lg">
+          <div class="caption">
+              <a href="{!! url('sadmin/user/create') !!}" class="btn btn-primary btn-add"><i class="fa fa-plus"></i> Add New User</a>
+          </div>
+      </div>
+      <div class="row clearfix">
+        <div class="col-lg-12">
+          <div class="card">
 
+           <div class="body">
+            <table class="table table-bordered table-striped table-hover dataTable js-exportable" cellspacing="0"  id="posts" width="100%">
 
-
-
-
-                         <div class="row">
-            <div class="col-xs-12">
-                <div class="box-body table-responsive">
-
-
-                      <table class="table table-bordered" id="posts">
-                    <thead>  
-                              <tr class="bg-primary">
-                               <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                    </thead> 
-                    <tbody>
-                        <tr>
-                            <td>helloo</td>
-                            <td>hellooo2</td>
-                           
-                        </tr>
-                    </tbody>            
-               </table>
-
-
-                </div>
-            </div>
+              <thead>  
+                <tr class="">
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+              </thead> 
+                         
+            </table>
+          </div>
         </div>
-                </div>
-                <!-- /.end -->
-            </div>
-        </div>
+      </div>
     </div>
-    <!-- /.container-fluid -->
-    </div>
-    <!-- /#page-wrapper -->
+  </div>
+</div>
+</div>
+
 
 @stop
 
